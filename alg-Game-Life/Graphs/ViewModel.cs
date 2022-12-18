cff374bd99f2d3926959f7d66f54654ec5ddf88b
Mikhail <m-5321e@yandex.com>
2022-12-18 05:56:34 +0500

    Добавлена отрисовка графика (RealTimeGraphX)

---
+++ alg-Game-Life/Graphs/ViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Media;
+using RealTimeGraphX;
+using RealTimeGraphX.DataPoints;
+using RealTimeGraphX.WPF;
+
+namespace alg_Game_Life.Graphs
+{
+    public class ViewModel
+    {
+        //Graph controller with timespan as X axis and double as Y.
+        public WpfGraphController<TimeSpanDataPoint, DoubleDataPoint> Controller { get; set; }
+
+        public ViewModel()
+        {
+            Controller = new WpfGraphController<TimeSpanDataPoint, DoubleDataPoint>();
+            Controller.Range.MinimumY = 0;
+            Controller.Range.MaximumY = int.MaxValue;
+            Controller.Range.MaximumX = TimeSpan.FromSeconds(10);
+            Controller.Range.AutoY = true;
+            Controller.Range.AutoYFallbackMode = GraphRangeAutoYFallBackMode.MinMax;
+
+            Controller.DataSeriesCollection.Add(new WpfGraphDataSeries()
+            {
+                Name = "Series Name",
+                Stroke = Color.FromRgb(113, 96, 232),
+                Fill = new SolidColorBrush(Color.FromArgb(50, 113, 96, 232)),
+                StrokeThickness = 1
+            });
+
+            Start();
+        }
+
+        /// <summary> Запуск отрисовки графика </summary>
+        private void Start()
+        {
+            var thread = new Thread(() =>
+            {
+                int index = 1;
+                while (true)
+                {
+                    var y = index;
+                    var x = DateTime.Now.TimeOfDay;
+
+                    Controller.PushData(x, y);
+
+                    Thread.Sleep(300);
+                    index++;
+                }
+            });
+
+            thread.Start();
+        }
+    }
+}
---
+++ alg-Game-Life/Graphs/WpfGraphControl.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using RealTimeGraphX;
+
+namespace alg_Game_Life.Graphs
+{
+    public class WpfGraphControl : Control
+    {
+        /// <summary> Gets or sets the graph controller. </summary>
+        public IGraphController Controller
+        {
+            get => (IGraphController) GetValue(ControllerProperty);
+            set => SetValue(ControllerProperty, value);
+        }
+
+        public static readonly DependencyProperty ControllerProperty =
+            DependencyProperty.Register("Controller",
+                                        typeof(IGraphController),
+                                        typeof(WpfGraphControl),
+                                        new PropertyMetadata(null));
+
+        /// <summary> Gets or sets a value indicating whether to display a tool tip with the current cursor value. </summary>
+        public bool DisplayToolTip
+        {
+            get => (bool) GetValue(DisplayToolTipProperty);
+            set => SetValue(DisplayToolTipProperty, value);
+        }
+
+        public static readonly DependencyProperty DisplayToolTipProperty =
+            DependencyProperty.Register("DisplayToolTip", typeof(bool), typeof(WpfGraphControl), new PropertyMetadata(true));
+
+        /// <summary> Gets or sets the string format for the X Axis. </summary>
+        public string StringFormatX
+        {
+            get => (string) GetValue(StringFormatXProperty);
+            set => SetValue(StringFormatXProperty, value);
+        }
+
+        public static readonly DependencyProperty StringFormatXProperty =
+            DependencyProperty.Register("StringFormatX", typeof(string), typeof(WpfGraphControl), new PropertyMetadata("0"));
+
+        /// <summary> Gets or sets the string format for the Y Axis. </summary>
+        public string StringFormatY
+        {
+            get => (string) GetValue(StringFormatYProperty);
+            set => SetValue(StringFormatYProperty, value);
+        }
+
+        public static readonly DependencyProperty StringFormatYProperty =
+            DependencyProperty.Register("StringFormatY", typeof(string), typeof(WpfGraphControl), new PropertyMetadata("hh:mm:ss"));
+
+        /// <summary> Initializes the <see cref="WpfGraphControl"/> class. </summary>
+        static WpfGraphControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WpfGraphControl), new FrameworkPropertyMetadata(typeof(WpfGraphControl)));
+        }
+    }
+}
